f82ffe02c24c88e7f3f0e9a0b84eb75a7847edb2
robbyhr <45536332+robbyhr@users.noreply.github.com>
2020-03-04 11:04:20 +0700

    update

---
+++ ConstraintLayout/app/src/main/java/robby/hermando/constraintlayout/MainActivity.kt
@@ -0,0 +1,13 @@
+package robby.hermando.constraintlayout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,4 @@
+Credit by : Robby Hermando Remen
+
+please give a star to appreciate me
+
